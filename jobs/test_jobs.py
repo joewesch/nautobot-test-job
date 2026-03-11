a4f9724eb56b737c3a436df96deeed9630fdde9c
Joe Wesch <10467633+joewesch@users.noreply.github.com>
2026-03-11 12:04:51 -0500

    Adds random sleep job

---
+++ jobs/test_jobs.py
@@ -0,0 +1,16 @@
+import time
+from random import randint
+
+from nautobot.apps.jobs import Job
+
+name = "Test Jobs"
+
+
+class RandomSleep(Job):
+    class Meta:
+        name = "Randomly sleep and end."
+        description = "Randomly sleep an amount of seconds between 1 and 10 and then complete/"
+
+    def run(self):
+        time.sleep(randint(1, 10))
+        self.logger.info("Done")
