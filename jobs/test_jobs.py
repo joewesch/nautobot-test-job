53e30a4ef814f0e1a6a2a4cf948c36d22305f690
Joe Wesch <joe.wesch@networktocode.com>
2026-06-24 11:04:35 -0500

    Added job hook receiver

--- jobs/test_jobs.py
+++ jobs/test_jobs.py
@@ -1,7 +1,7 @@
 import time
 from random import randint
 
-from nautobot.apps.jobs import Job, register_jobs
+from nautobot.apps.jobs import Job, JobHookReceiver, register_jobs
 
 name = "Test Jobs"
 
@@ -20,3 +20,22 @@ class RandomSleep(Job):
 
 
 register_jobs(RandomSleep)
+
+
+class JobHookReporter(JobHookReceiver):
+    """Logs all details about the changed object and ends."""
+
+    def receive_job_hook(self, change, action, changed_object):
+        self.logger.info(f"ObjectChange: {change}", extra={"object": changed_object})
+
+        snapshots = change.get_snapshots()
+        self.logger.info("DIFF: %s", snapshots["differences"])
+
+        self.logger.info(f"Action: {action}")
+
+        if changed_object:
+            # Changed object is None when the object is deleted.
+            self.logger.info(f"Changed object: {changed_object}", extra={"object": changed_object})
+
+
+register_jobs(JobHookReporter)
